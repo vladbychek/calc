7828b66bf4a73143466ce88fea55347c570f7f14
Vlad <vladbychek3@gmail.com>
2022-09-27 19:07:32 +0300

    start

---
+++ task.js
@@ -0,0 +1,36 @@
+
+
+
+
+function addOne() {
+    let a = 1
+    result.append(a)
+}
+function addTwo() {
+    let b = 2
+    result.append(b) 
+}
+function reset() {
+     
+}
+
+
+
+document.getElementById('btn_one').addEventListener('click', addOne)
+document.getElementById('btn_two').addEventListener('click', addTwo)
+document.getElementById('btn_three').addEventListener('click', )
+document.getElementById('btn_four').addEventListener('click', )
+document.getElementById('btn_five').addEventListener('click', )
+document.getElementById('btn_six').addEventListener('click', )
+document.getElementById('btn_seven').addEventListener('click', )
+document.getElementById('btn_eigth').addEventListener('click', )
+document.getElementById('btn_nine').addEventListener('click', )
+document.getElementById('btn_zero').addEventListener('click', )
+document.getElementById('btn_plus').addEventListener('click', )
+document.getElementById('btn_minus').addEventListener('click',)
+document.getElementById('btn_umnogit').addEventListener('click', )
+document.getElementById('btn_delit').addEventListener('click', )
+document.getElementById('btn_percent').addEventListener('click', )
+document.getElementById('btn_root').addEventListener('click', )
+document.getElementById('btn_reset').addEventListener('click', )
+document.getElementById('btn_result').addEventListener('click', )
